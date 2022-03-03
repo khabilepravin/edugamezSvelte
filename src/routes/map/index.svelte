4b770241070fcd55a5be9956b65ea8b90c6df134
i <i@is-MacBook-Pro.lan>
2022-03-03 20:57:06 +1100

    Basic geocharts integration

--- src/routes/map/index.svelte
+++ src/routes/map/index.svelte
@@ -1,36 +1,27 @@
 <script>
-    import { GoogleCharts } from "google-charts";
+	import { GoogleCharts } from 'google-charts';
+	import { onMount } from 'svelte';
 
-    GoogleCharts.load(drawGeoChart, 
-    {    
-        "packages": ['geochart'],
-    "mapsApiKey": import.meta.env.VITE_ENV_mapAPIKey
-    });
+	onMount(() => {
+		GoogleCharts.load(drawGeoChart, {
+			packages: ['geochart'],
+			mapsApiKey: import.meta.env.VITE_ENV_mapAPIKey
+		});
 
-    function drawGeoChart() {
- 
- /* Geo Chart */
- const geo_1_data = GoogleCharts.api.visualization.arrayToDataTable([
-     ['State', 'Spend'],
-     ['ID', {v:120000, f: '$120,000'}],
-     ['CO', {v:567135, f: '$567,135'}],
-     ['FL', {v:220000, f: '$220,000'}],
-     ['NY', {v:1120000, f: '$1,120,000'}],
-     ['CA', {v:5120000, f: '$5,120,000'}],
-     ['AK', {v:101000, f: '$101,000'}],
-     ['AZ', {v:311030, f: '$311,030'}]
- ]);
- const geo_1_options = {
-     region: 'US',
-     resolution: 'provinces',
-     is3D: true,
-     legend: {
-         numberFormat:'$###,###'
-     }
- }
- const geo_1_chart = new GoogleCharts.api.visualization.GeoChart(document.getElementById('geo_1_chart'));
- geo_1_chart.draw(geo_1_data, geo_1_options);
-}
+		function drawGeoChart() {
+			/* Geo Chart */
+			const geo_1_data = GoogleCharts.api.visualization.arrayToDataTable([['Country'], ['India']]);
+			const geo_1_options = {
+				domain: 'IN',
+				defaultColor: '#0000FF',
+				enableRegionInteractivity: false
+			};
+			const geoChart = new GoogleCharts.api.visualization.GeoChart(
+				document.getElementById('geoChart')
+			);
+			geoChart.draw(geo_1_data, geo_1_options);
+		}
+	});
 </script>
 
-<div id="geo_1_chart"></div>
+<div id="geoChart" />
