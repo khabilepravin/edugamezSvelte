ffcd428c8207c21bb2e608b7bc7ce59c866b2b62
Pravin Khabile <z1195478@wowcorp.com.au>
2022-03-03 13:46:54 +1100

    CSS class for no-audio element and animation for audio control

--- src/routes/spelling/AudioPlayer.svelte
+++ src/routes/spelling/AudioPlayer.svelte
@@ -1,5 +1,6 @@
   <script>
     import { onMount } from 'svelte'
+    import { fade } from 'svelte/transition';
   
     export let src    
     export let autoPlay
@@ -12,14 +13,22 @@
   
   <div>
     {#if (src === null)}
+    <div class="no-audio-div">
       <h6>Not available</h6>
+    </div>
     {:else}    
       <audio
           bind:this={player}
           {src}
+          transition:fade
           controls
-          autoplay={autoPlay}>
-          <!-- <track kind="captions" /> -->
+          autoplay={autoPlay}>         
       </audio>
     {/if}
   </div>
+
+  <style>
+    .no-audio-div {
+      height: 60px;
+    }
+  </style>
