5d06c16a5ee6e05c4be0d3398dc48fc995b1012b
pravinkhabile <pravin.khabile@leapdev.io>
2022-09-24 14:01:06 +1000

    Intermediate page to select the level of difficulty

--- src/routes/spelling/index.svelte
+++ src/routes/spelling/index.svelte
@@ -1,150 +1,30 @@
-<script>
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';	
-
-	// local imports
-	import AudioPlayer from './AudioPlayer.svelte';
-	import { spellingUserAnswers } from '../../lib/store';
-	import { getUrlByRegion } from './utility';
-	import Progress from '$lib/progress.svelte';
-	import { percentage } from '$lib/percent';
-	import RegionSelector from '$lib/regionselector.svelte';
-
-	// public props
-	export let spellingDataV2;
-
-	// local props
-	let currentRegion = 'en-AU'; //TODO: Interpret this from the browser locale
-	let currentIndex = 0;
-	let wordAudioUrl;
-	let definitonAudioUrl;
-	let exampleAudioUrl;
-	let partsOfTheSpeech = [];
-	let definitionAndExampleFor;
-	let spelledAnswer = '';
-	const maxRecordsInATest = 10;
-	let percentComplete = 0;
-	let timer;
-
-	// store variables
-	$spellingUserAnswers = [];
-
-	// lifecycle hooks
-	onMount(async () => {
-		setComponentData(spellingDataV2[currentIndex]);
-	});
-
-	// private functions
-	const setComponentData = (currentWordInstance) => {
-		wordAudioUrl = getUrlByRegion(currentWordInstance.WordAudios, currentRegion);
-		definitonAudioUrl = getUrlByRegion(currentWordInstance.DefinitionAudios, currentRegion);
-		exampleAudioUrl = getUrlByRegion(currentWordInstance.ExampleAudios, currentRegion);
-		partsOfTheSpeech = currentWordInstance.PartsOfTheSpeech;
-		definitionAndExampleFor = currentWordInstance.DefinitionAndExampleFor;
-	};
-
-	const getUserAnswer = (originalWord, userAnswer) => {
-		let isCorrect = originalWord.Word.trim().toLowerCase() === userAnswer.trim().toLowerCase();
-
-		return {
-			actualWord: originalWord.Word,
-			userAnswer: userAnswer,
-			isCorrect: isCorrect
-		};
-	};
-
-	// Event handlers
-	const handleNext = () => {
-		percentComplete = percentage(currentIndex + 1, maxRecordsInATest);
-		let currentWordData = spellingDataV2[currentIndex];
-
-		$spellingUserAnswers = [...$spellingUserAnswers, getUserAnswer(currentWordData, spelledAnswer)];
-
-		if (currentIndex === 9) {
-			goto('/spelling/result');
-		} else {
-			// Move to next
-			currentIndex = currentIndex + 1;
-			currentWordData = spellingDataV2[currentIndex];
-			setComponentData(currentWordData);
-			spelledAnswer = '';
-		}
-	};
-
-	const handleRegionChange = (event) => {
-		currentRegion = event.detail;
-		setComponentData(spellingDataV2[currentIndex]);
-	};
-</script>
-
 <svelte:head>
 	<title>Spelling Game</title>
 </svelte:head>
-<div class="text-center">	
+
+<div class="text-center">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm">
-			<img src="images/spelling-icon.PNG" class="iconImageSize" alt="Spelling icon"/><h6>Spelling Game</h6>
-			</div>	
-		</div>
-		<div class="row">
-			<div class="col-sm">
-				<RegionSelector on:regionChanged={handleRegionChange} selectedRegion={currentRegion} />
-			</div>
-		</div>
-	</div>
-	<h6>Word</h6>
-
-	<AudioPlayer src={wordAudioUrl} autoPlay="true" />
-	<h6>Definition</h6>
-	<AudioPlayer src={definitonAudioUrl} autoPlay={null} />
-	<h6>Example</h6>
-	<AudioPlayer src={exampleAudioUrl} autoPlay={null} />
-
-	<div class="container">
-		<div class="row">
-			<div class="col-sm">
-				{#each partsOfTheSpeech as part}
-					{#if part === definitionAndExampleFor}
-						<strong>{part} &nbsp;</strong>
-					{:else}
-						{part} &nbsp;
-					{/if}
-				{/each}
+				<h5>Select Level of Difficulty</h5>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-sm">
-				<input
-					type="text"
-					class="form-control"
-					placeholder="Spelling"
-					spellcheck="false"
-					autocomplete="off"
-					autocorrect="off"
-					autocapitalize="none"					
-					bind:value={spelledAnswer}
-				/>
+				<nav>
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="/spelling/game?difficulty=low">Low</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/spelling/game?difficulty=medium">Medium</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="/spelling/game?difficulty=high">High</a>
+						</li>
+					</ul>
+				</nav>
 			</div>
 		</div>
-		<Progress
-			currentProgress={percentComplete}
-			currentItemNumber={currentIndex + 1}
-			totalItems={maxRecordsInATest}
-		/>
 	</div>
 </div>
-<button
-	on:click={handleNext}	
-	value="Next"
-	class="btn btn-primary mt-3 float"
-	>Next
-</button>
-
-<style>
-	.float {
-		position: fixed;
-		bottom: 40px;
-		right: 40px;
-	}
-</style>
