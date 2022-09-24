948da40de3c1fe7184b15e9e91f2456dba4cc048
pravinkhabile <pravin.khabile@leapdev.io>
2022-09-24 17:08:19 +1000

    Home link fix, removed login button

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,7 +1,4 @@
-<script>
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
-</script>
+
 
 <nav class="navbar navbar-expand-lg fixed-top navbar-light">
 	<div class="container">
@@ -12,7 +9,7 @@
 		<div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
 			<ul class="navbar-nav ml-auto">
 				<li class="nav-item">
-					<a class="nav-link page-scroll" href="index.html#header"
+					<a class="nav-link page-scroll" href="./"
 						>Home <span class="sr-only">(current)</span></a
 					>
 				</li>
@@ -23,9 +20,9 @@
 					<a class="nav-link page-scroll" href="#contact">Contact</a>
 				</li>
 			</ul>
-			<span class="nav-item">
+			<!-- <span class="nav-item">
 				<a class="btn-outline-sm">Log In</a>
-			</span>
+			</span> -->
 		</div>
 	</div>
 </nav>
