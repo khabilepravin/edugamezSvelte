93452d0650ac9e7cae35164881c93e0a257a08c7
pravinkhabile <pravin.khabile@leapdev.io>
2023-07-07 15:18:11 +1000

    About us changed

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,25 +4,35 @@
 <div class="container">
 	<div class="row">
 		<div class="col-lg-12">
-			<h3 class="h2-heading">About us</h3>
+			<h3 class="h2-heading float-left">ABOUT US</h3>
 		</div>
 		<!-- end of col -->
 	</div>
-    <div class="row">
+	<div class="row">
 		<div class="col-lg-12">
 			<p>
-                We believe spelling is cruicial for writing as well as for reading.
-                Spelling is a key aspect of vocabulary development, this, in turn supports reading comprehension and enhances choices made when composing written texts.
-            </p>
-            <br/>
-            <p>
-                And just like everything else, practice is the only way to get better at it.                
-            </p>
-            <br/>
-            <p>
-                MaxSpelling is a platform with ever increasing content to support learners of all levels to get better at spelling.
-            </p>
+				We believe spelling is cruicial for writing as well as for reading. Spelling is a key aspect
+				of vocabulary development, this, in turn supports reading comprehension and enhances choices
+				made when composing written texts.
+			</p>
+			<p>
+				The way we have structured the content enables you to enhanced listening comprehension,
+				listening to pronunciations enables you to train your ear to understand different accents,
+				intonations, and speech patterns. This improves your overall listening comprehension skills
+				and allows you to better understand native speakers.
+			</p>
+			<p>And just like everything else, practice is the only way to get better at it.</p>
+			<p>
+				MaxSpelling is a platform with ever increasing content to support learners of all levels to
+				get better at spelling.
+			</p>
 		</div>
 		<!-- end of col -->
 	</div>
 </div>
+
+<style>
+    p{
+        padding-bottom: 8px;
+    }
+</style>
