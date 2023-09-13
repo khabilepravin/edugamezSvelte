a8aacb35180435d1aded849141655060673856bd
pravinkhabile <pravin.khabile@leapdev.io>
2023-09-13 12:01:05 +1000

    Basic stuff for offline support and manifest inclusion

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,45 +1,58 @@
-// Define a cache name for your app's assets
-const CACHE_NAME = 'max-spelling-cache';
+///<reference types="@sveltejs/kit"/>
+import { build, files, version } from '$service-worker';
 
-// List of static assets to cache
-const staticAssets = [
-  '/',
-  '/app.html',
-  '/global.css',
-  '/build/bundle.css',
-  '/build/bundle.js',
-  // Add more assets as needed
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+	...build, // the app itself
+	...files // everything else in the static directory
 ];
 
-// Install the service worker and cache the static assets
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(staticAssets);
-    })
-  );
+	// Create a new cache and add all files to it
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUnitl(addFilesToCache());
 });
 
-// Serve cached assets when offline
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Clean up old caches when a new service worker activates
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) await caches.delete(key);
+		}
+	}
+
+	event.waitUnitl(deleteOldCaches());
+});
+
+self.addEventListener('fetch', (event) => {
+	// ignore POST requests
+	if (event.request.method !== 'GET') {
+		return;
+	}
+
+	async function respond() {
+		const url = new URL(event.request.url);
+		const cache = await caches.open(CACHE);
+
+		if (ASSETS.includes(url.pathname)) {
+			return cache.match(event.request);
+		}
+
+		try {
+			const response = await fetch(event.request);
+			if (response.status === 200) {
+				cache.put(event.request, response.clone());
+			}
+
+			return response;
+		} catch {
+			return cache.match(event.request);
+		}
+	}
+
+  event.respondWith(respond());
 });
